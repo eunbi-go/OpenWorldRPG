b0214d13ee2783152c0557075d34721e59383074
eunbi <geunbi38@gmail.com>
2023-06-21 17:43:57 +0900

    1. AddActorWorldOffset(), AddActorWorldRotation(), DeltaTime 을 활용한 객체의 이동/회전.
    2. 디버깅용 도형을 단일 프레임으로 그리는 매크로 추가.

--- UAG/Source/UAG/DebugMacros.h
+++ UAG/Source/UAG/DebugMacros.h
@@ -4,12 +4,24 @@
 
 #define DRAW_SPHERE(location) if (GetWorld()) DrawDebugSphere(GetWorld(), location, 25.f, 12, FColor::Red, true)
 
+#define DRAW_SPHERE_SINGLE_FRAME(location) if (GetWorld()) DrawDebugSphere(GetWorld(), location, 25.f, 12, FColor::Red, false, -1.f);
+
 #define DRAW_LINE(startLocation, endLocation) if (GetWorld()) DrawDebugLine(GetWorld(), startLocation, endLocation, FColor::Blue, true, -1.f, 0, 1.f);
 
+#define DRAW_LINE_SINGLE_FRAME(startLocation, endLocation) if (GetWorld()) DrawDebugLine(GetWorld(), startLocation, endLocation, FColor::Blue, false, -1.f, 0, 1.f);
+
 #define DRAW_POINT(location) if (GetWorld()) DrawDebugPoint(GetWorld(), location, 15.f, FColor::Green, true);
+
+#define DRAW_POINT_SINGLE_FRAME(location) if (GetWorld()) DrawDebugPoint(GetWorld(), location, 15.f, FColor::Green, false, -1.f);
 
 #define DRAW_VECTOR(startLocation, endLocation) if (GetWorld()) \
 	{ \
 		DrawDebugLine(GetWorld(), startLocation, endLocation, FColor::Black, true, -1.f, 0, 1.f); \
 		DrawDebugPoint(GetWorld(), endLocation, 15.f, FColor::Black, true); \
+	}
+
+#define DRAW_VECTOR_SINGLE_FRAME(startLocation, endLocation) if (GetWorld()) \
+	{ \
+		DrawDebugLine(GetWorld(), startLocation, endLocation, FColor::Black, false, -1.f, 0, 1.f); \
+		DrawDebugPoint(GetWorld(), endLocation, 15.f, FColor::Black, false, -1.f); \
 	}
