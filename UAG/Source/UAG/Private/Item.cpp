350c87551fdee25eac614a359345766a7bb521a3
eunbi <geunbi38@gmail.com>
2023-06-28 00:50:32 +0900

    EnhancedInput 을 활용한 플레이어 이동, 카메라 회전 구현.

--- UAG/Source/UAG/Private/Item.cpp
+++ UAG/Source/UAG/Private/Item.cpp
@@ -8,6 +8,13 @@ AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	// ItemMeshComponent: 기본 생성을 위한 내부 이름.
+	itemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	
+	// 새로 만든 메시의 주소를 저장하기 위해 루트 컴포넌트를 재할당한다.
+	// 이전 루트 컴포넌트가 저장하고 있던 주소는 자동으로 삭제되고 itemMesh 의 주소로 설정된다.
+	// 가비지 컬렉션이 루트 컴포넌트가 더이상 이전 주소를 가리키지 않는다는 것을 확인하기 때문.
+	RootComponent = itemMesh;
 }
 
 void AItem::BeginPlay()
@@ -15,17 +22,21 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 }
 
+float AItem::TransformedSin()
+{
+	return amplitude * FMath::Sin(runningTime * timeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return amplitude * FMath::Cos(runningTime * timeConstant);
+}
+
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 	runningTime += DeltaTime;
-
-	float deltaZ = amplitude * FMath::Sin(runningTime * timeConstant);
-
-	AddActorWorldOffset(FVector(0.f, 0.f, deltaZ));
-
-	DRAW_SPHERE_SINGLE_FRAME(GetActorLocation());
-	DRAW_VECTOR_SINGLE_FRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 }
 
+ 
--- UAG/Source/UAG/Public/Item.h
+++ UAG/Source/UAG/Public/Item.h
@@ -17,13 +17,35 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	// 진폭
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine params")
+		float	amplitude = 0.25f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine params")
+		float	timeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	template<typename T>
+	T Avg(T first, T second);
 
 private:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float	runningTime = 0.f;
 
-	// 진폭
-	float	amplitude = 0.25f;
-
-	float	timeConstant = 5.f;
+	// UPROPERTY(): 가비지 컬렉션에 참여.
+	// VisibleAnywhere: static mesh 를 변경하기 위해 해당 포인터를 블루프린트 에디터에 노출시킴.
+	UPROPERTY(VisibleAnywhere)
+	// 컴포넌트에 대한 포인터 생성.
+	UStaticMeshComponent* itemMesh;
 };
+
+template<typename T>
+inline T AItem::Avg(T first, T second)
+{
+	return (first + second) / 2;
+}
